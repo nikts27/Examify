f8fb221d319b2e5da62c359eb4211227a9259186
nikts27 <128230420+nikts27@users.noreply.github.com>
2024-11-01 16:04:54 +0200

    Update Professor.java

--- exams/src/main/java/com/example/exams/model/Professor.java
+++ exams/src/main/java/com/example/exams/model/Professor.java
@@ -3,17 +3,27 @@ package com.example.exams.model;
 import com.example.exams.domain.USER_ROLE;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
+
+import java.util.List;
 
 @Entity
 @Data
-@EqualsAndHashCode(callSuper = true)
 @DiscriminatorValue("PROFESSOR")
 public class Professor extends User {
+
+    @ManyToMany(mappedBy = "professors")
+    private List<Exam> examsCreated;
 
     public Professor() {
         super();
         this.role = USER_ROLE.ROLE_PROFESSOR;
+    }
+
+
+    @Override
+    public boolean equals(User user) {
+        return this.username.equals(user.getUsername());
     }
 }
