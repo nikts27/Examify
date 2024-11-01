ab19810aa37c49eb1ae166fbd12d3075d0de7ae9
nikts27 <128230420+nikts27@users.noreply.github.com>
2024-11-01 16:05:59 +0200

    Update Student.java

--- exams/src/main/java/com/example/exams/model/Student.java
+++ exams/src/main/java/com/example/exams/model/Student.java
@@ -3,16 +3,27 @@ package com.example.exams.model;
 import com.example.exams.domain.USER_ROLE;
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
+
+import java.util.List;
 
 @Entity
 @Data
-@EqualsAndHashCode(callSuper = true)
 @DiscriminatorValue("STUDENT")
 public class Student extends User {
+
+    @ManyToMany(mappedBy = "assignedStudents")
+    private List<Exam> examsEnrolled;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
+    private List<Submission> submissions;
 
     public Student() {
         super();
         this.role = USER_ROLE.ROLE_STUDENT;
+    }
+
+    @Override
+    public boolean equals(User user) {
+        return this.username.equals(user.getUsername());
     }
 }
