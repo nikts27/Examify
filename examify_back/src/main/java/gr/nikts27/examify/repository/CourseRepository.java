1ed37949d1cf6b36f88753ef17fc1a1925f81a6d
nikts27 <128230420+nikts27@users.noreply.github.com>
2025-01-08 02:12:08 +0200

    Update CourseRepository.java

--- examify_back/src/main/java/gr/nikts27/examify/repository/CourseRepository.java
+++ examify_back/src/main/java/gr/nikts27/examify/repository/CourseRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-/**
- * @author Cleavest on 3/1/2025
- */
 public interface CourseRepository extends MongoRepository<Course, String> {
 
     List<Course> findByProfessorsContaining(String professorId);
